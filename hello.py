d87c1ec17ef47e94561cc696bea382c860fe5ecb
uannabi <zahid_sc@hotmail.fr>
2022-04-07 19:41:16 +0600

    test 2

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import fire
+from rdflib.graph import Graph
 
 def hello(name="World"):
+  g = Graph()
+  g.parse("demo.nt", format="nt")
   return "Hello %s!" % name
 
 if __name__ == '__main__':
---
+++ helol.py
@@ -0,0 +1,4 @@
+from rdflib.graph import Graph
+g = Graph()
+g.parse("/Users/khoundokarzahid/Stack/github/AprilOptimizy/AprilOptimzy/data/data/redirects.nt", format="nt")
+
